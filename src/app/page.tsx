a1d2041da6e5bc4c356a9c5cb13c72b9dc6b1926
Jonathan Rios <jonarios2511@gmail.com>
2025-10-08 21:00:37 +0000

    @media (max-width: 767.98px) {     .contact-info {         font-size: 13

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,24 +26,19 @@ export default function Home() {
 
     let i = 0;
     const startTyping = () => {
-      typingIntervalRef.current = setInterval(() => {
-        if (i < fullText.length) {
-          setTypedText((prev) => prev + fullText.charAt(i));
-          i++;
-        } else {
-          if (typingIntervalRef.current) {
-            clearInterval(typingIntervalRef.current);
-          }
-        }
-      }, 100);
+      if (i < fullText.length) {
+        setTypedText((prev) => prev + fullText.charAt(i));
+        i++;
+        typingIntervalRef.current = setTimeout(startTyping, 100);
+      }
     };
     
-    const timeoutId = setTimeout(startTyping, 100);
+    const initialTimeoutId = setTimeout(startTyping, 100);
 
     return () => {
-      clearTimeout(timeoutId);
+      clearTimeout(initialTimeoutId);
       if (typingIntervalRef.current) {
-        clearInterval(typingIntervalRef.current);
+        clearTimeout(typingIntervalRef.current);
       }
     };
   }, [fullText]);
@@ -61,7 +56,7 @@ export default function Home() {
         <LanguageSwitcher />
       </div>
       <EtherealCanvas />
-      <div className="relative md:px-[10rem] z-10 justify-center text-center text-foreground pointer-events-none">
+      <div className="relative z-10 justify-center text-center text-foreground pointer-events-none">
         <h1 className="text-xl lg:text-4xl font-headline font-bold mb-0 mt-[5rem] md:mt-[10rem]">
           {typedText}
           <span className="typing-cursor">|</span>
@@ -75,7 +70,7 @@ export default function Home() {
       <Image src="/arrow.svg" alt="Logo" width={75} height={75} className="md:w-[120px] md:h-[120px] flex-center my-[1rem] md:my-[3rem] rotate-[-0.785398163rad] md:rotate-0" />
 
       <footer className="w-full">
-        <div className="w-full max-w-[91%] flex items-center justify-between mx-auto uppercase text-[15px] lg:text-[1.1rem] tracking-[0.5px] text-center">
+        <div className="w-full max-w-[91%] flex flex-col lg:flex-row items-center justify-between mx-auto uppercase text-[13px] lg:text-[1.1rem] tracking-[0.5px] text-center lg:space-x-4 space-y-2 lg:space-y-0">
             <a href="https://wa.me/8444609592" target="_blank" rel="noopener noreferrer" className=" hover:underline whitespace-nowrap">844 460 95 92</a>
             <a href="https://maps.app.goo.gl/EXNN1qBdzxh4VsoJ7" target="_blank" rel="noopener noreferrer" className=" hover:underline">Matamoros 216, Zona Centro, 25790 Monclova, Coah. MX</a>
             <a href="mailto:Hola@Ideomētrica.com" className=" hover:underline">Hola@Ideomētrica.com</a>
