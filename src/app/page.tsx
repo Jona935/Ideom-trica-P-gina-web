dd5fdedaafb2daa55d3939554af12b85fc029689
Jonathan Rios <jonarios2511@gmail.com>
2025-10-08 17:50:21 +0000

    agrerga mas espacion entre la informacion de contacto del footer cuando

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -59,8 +59,8 @@ export default function Home() {
       </div>
       <Image src="/arrow.svg" alt="Logo" width={75} height={75} className="md:w-[150px] md:h-[150px] flext-center pt-5 rotate-[-0.785398163rad] md:rotate-0" />
 
-      <footer className="relative z-10 w-full pt-8 pb-4">
-        <div className="max-w-4xl mx-auto flex flex-col md:flex-row justify-between items-center text-foreground text-base md:text-lg space-y-4 md:space-y-0">
+      <footer className="relative z-10 w-full pt-[4rem]">
+        <div className="max-w-4xl mx-auto flex flex-col md:flex-row justify-between items-center text-foreground text-base md:text-lg space-y-4 md:space-y-0 md:space-x-4">
             <a href="https://wa.me/8444609592" target="_blank" rel="noopener noreferrer" className="hover:underline whitespace-nowrap text-center">844 460 95 92</a>
             <a href="https://maps.app.goo.gl/EXNN1qBdzxh4VsoJ7" target="_blank" rel="noopener noreferrer" className="text-center hover:underline">Matamoros 216, Zona Centro, 25790 Monclova, Coah. MX</a>
             <a href="mailto:Hola@Ideomētrica.com" className="hover:underline text-center">Hola@Ideomētrica.com</a>
